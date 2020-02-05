22daec225548101412922d5ab80ce1f03ebcc047
Arsenij <arsendk@gmail.com>
2020-02-05 21:44:08 +0200

    added text to Main

--- src/lt/itacademy/java/secondDay/Main.java
+++ src/lt/itacademy/java/secondDay/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello world");
-	System.out.println("after commit test");
+	System.out.println("This is feature branch");
     }
 }
